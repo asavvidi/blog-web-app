293b09f45fd8187f53e3a3b8454fbc58b768b48c
asavvidi <asavvidi@csd.auth.gr>
2024-12-01 00:47:14 +0200

    Almost finished the structure and styling of create blog page and modify the footer

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
 });
 
 app.listen(port, () => {
