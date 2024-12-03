3a4ac333f81b92f664fef00b3c59aae4ed03e317
asavvidi <asavvidi@csd.auth.gr>
2024-12-04 01:06:56 +0200

    Finished the create post functionality and how to show the posts in a dynamic way

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const blogsContent = [
-  "Hello hwokdokoss",
-  "dssasaadfaff",
-  "dsdasffffffegeeefgeggg",
+let blogPosts = [
+  {
+    title:
+      "War latest: Putin's plane involved in illegally deporting Ukrainian children",
+    description:
+      "The new NATO chief has warned Vladimir Putin is ramping up reckless actions and is using Ukraine as a 'testing ground' for missiles. Meanwhile, US-backed research alleges the Kremlin leader's presidential plane has been involved in deporting Ukrainian children to Russia.",
+  },
+  {
+    title: "Ukraine accelerating missile production, Zelensky says.",
+    description:
+      'There is a "significant volume" of new and long-term orders for first-person-view (FPV) drones, reconnaissance drones, long-range drones, and missile-drones, President Volodymyr Zelensky said.',
+  },
 ];
-
 app.get("/", (req, res) => {
-  res.render("index.ejs", { content: blogsContent });
+  res.render("index.ejs", { content: blogPosts });
 });
 
 app.get("/create", (req, res) => {
@@ -23,6 +30,17 @@ app.get("/create", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const newBlogPost = {
+    title: req.body["blogTitle"],
+    description: req.body["blogDescription"],
+  };
+
+  blogPosts.push(newBlogPost);
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
